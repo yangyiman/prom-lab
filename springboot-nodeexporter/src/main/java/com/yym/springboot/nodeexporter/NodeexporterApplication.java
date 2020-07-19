e6c67d70e975e624d7be2b1b0c168007b7319020
yangyiman <yangyimanbest@163.com>
2020-07-19 18:09:49 +0800

    init first

---
+++ springboot-nodeexporter/src/main/java/com/yym/springboot/nodeexporter/NodeexporterApplication.java
@@ -0,0 +1,11 @@
+package com.yym.springboot.nodeexporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodeexporterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NodeexporterApplication.class,args);
+    }
+}
